ccde65e3bbae3c0532188cbbcd5374e9f85e399e
DDKROFF <ddkroff@gmail.com>
2025-12-04 14:03:19 +0300

    Написаны тесты на slice

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,3 +12,26 @@ def test_get_nonexistent_index_without_default():
 
 def test_get_negative_index_returns_default():
     assert get([10, 20, 30], -1, default='default') == 'default'
+
+def test_my_slice_default():
+    arr = [1, 2, 3, 4, 5]
+    assert my_slice(arr) == arr
+
+def test_my_slice_start_and_end():
+    arr = [1, 2, 3, 4, 5]
+    assert my_slice(arr, 1, 3) == [2, 3]
+
+def test_my_slice_start_only():
+    arr = [1, 2, 3, 4, 5]
+    assert my_slice(arr, 2) == [3, 4, 5]
+
+def test_my_slice_end_only():
+    arr = [1, 2, 3, 4, 5]
+    assert my_slice(arr, end=3) == [1, 2, 3]
+
+def test_my_slice_empty_collection():
+    assert my_slice([]) == []
+
+def test_my_slice_end_greater_than_length():
+    arr = [1, 2, 3]
+    assert my_slice(arr, 1, 10) == [2, 3]
